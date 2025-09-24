be9d8ed7a7e568b207b03519644e36e6ecc1a8fd
AlexConroyWeithaler <124644596+AlexConroyWeithaler@users.noreply.github.com>
2025-09-24 19:59:49 +1200

    a

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -22,6 +22,7 @@ const SPEED = 6.0
 const HORIZONTAL_ACC = 2.0
 const JUMP_VELOCITY = 5
 var current_speed_x: float
+var current_speed_z: float
 func _physics_process(delta: float) -> void:
 	handle_animations()
 	# Add the gravity.
@@ -38,6 +39,7 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		curAnim = WALK
